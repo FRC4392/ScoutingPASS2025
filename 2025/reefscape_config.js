0af98001e1402047dec7c5a82e110fdc3c4290e3
bishmopp <105512272+bishmopp@users.noreply.github.com>
2025-02-28 12:39:13 -0500

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025MIMIL",
+      "defaultValue": "MIMIL",
       "required": "true"
     },
     { "name": "Match Level",
